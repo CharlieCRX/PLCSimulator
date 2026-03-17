40e0b67a137153fce77406c95bed412366d2b00f
chenruixin <2807504409@qq.com>
2026-03-17 19:47:55 +0800

    first step

---
+++ build/CMakeFiles/plc_simulator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/plc_simulator.dir/link.d"
+  "CMakeFiles/plc_simulator.dir/src/main.cpp.o"
+  "CMakeFiles/plc_simulator.dir/src/main.cpp.o.d"
+  "plc_simulator"
+  "plc_simulator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/plc_simulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
